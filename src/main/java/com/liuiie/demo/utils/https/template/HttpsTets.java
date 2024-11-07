bd84af9c4da9703937971d5dd6709ca7bde8ffe9
Liuiie <1012558791@qq.com>
2024-11-07 08:49:27 +0800

    【HttpUtils】 1.新增地址增加参数方法和测试用例

--- src/main/java/com/liuiie/demo/utils/https/template/HttpsTets.java
+++ src/main/java/com/liuiie/demo/utils/https/template/HttpsTets.java
@@ -4,7 +4,9 @@ import com.alibaba.fastjson.JSON;
 import com.liuiie.demo.utils.https.HttpUtils;
 import org.springframework.core.ParameterizedTypeReference;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * HttpsTets
@@ -14,7 +16,25 @@ import java.util.List;
  */
 public class HttpsTets {
     public static void main(String[] args) throws Exception {
+        // 调用用例
         ParkResponse<List<String>> parkResponse = HttpUtils.doGet("", new ParameterizedTypeReference<ParkResponse<List<String>>>(){});
         System.out.println(JSON.toJSONString(parkResponse));
+
+        // 地址增加参数用例
+        Map<String, String> newParams = new HashMap<>();
+        newParams.put("param1", "value1");
+        newParams.put("param2", "value2");
+        String url1 = "https://app-rgtest.rizhaosteel.com:4443/pdisp/imc/mobile/";
+        String updatedUrl1 = HttpUtils.addQueryParameters(url1, newParams);
+        System.out.println(updatedUrl1);
+        String url2 = "https://app-rgtest.rizhaosteel.com:4443/pdisp/imc/mobile/#/dispatchToUrl";
+        String updatedUrl2 = HttpUtils.addQueryParameters(url2, newParams);
+        System.out.println(updatedUrl2);
+        String url3 = "https://app-rgtest.rizhaosteel.com:4443/pdisp/imc/mobile/#/dispatchToUrl?url=manufacturingDepartment";
+        String updatedUrl3 = HttpUtils.addQueryParameters(url3, newParams);
+        System.out.println(updatedUrl3);
+        String url4 = "https://app-rgtest.rizhaosteel.com:4443/pdisp/imc/mobile?url=manufacturingDepartment#dispatchToUrl";
+        String updatedUrl4 = HttpUtils.addQueryParameters(url4, newParams);
+        System.out.println(updatedUrl4);
     }
 }
