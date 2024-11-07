c025c2a91a85169da79669f972027dda0caea6cb
Liuiie <1012558791@qq.com>
2024-11-07 17:01:48 +0800

    【ListUtil】 1.增加拆分集合方法

--- src/main/java/com/liuiie/demo/utils/array/ListUtil.java
+++ src/main/java/com/liuiie/demo/utils/array/ListUtil.java
@@ -1,5 +1,7 @@
 package com.liuiie.demo.utils.array;
 
+import org.springframework.util.ObjectUtils;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -14,6 +16,43 @@ import java.util.stream.Stream;
  * @since 2024/9/25 14:17
  */
 public class ListUtil {
+    /**
+     * 将一个 List 拆分成多个列表
+     *
+     * @param originalList 原始列表
+     * @param chunkCount 拆分数量
+     * @return 拆分后的列表集合
+     * @param <T> 泛型
+     */
+    public static <T> List<List<T>> splitListIntoChunks(List<T> originalList, int chunkCount) {
+        List<List<T>> result = new ArrayList<>();
+        // 原始列表为空时直接返回空数组
+        if (ObjectUtils.isEmpty(originalList)) {
+            return result;
+        }
+        // 原始列表大小为1时直接返回单个数组
+        if (originalList.size() == 1) {
+            result.add(originalList);
+            return result;
+        }
+        int size = originalList.size();
+        // 每个子列表的基本大小
+        int chunkSize = size / chunkCount;
+        // 多余的元素数量
+        int remainder = size % chunkCount;
+        int start = 0;
+        for (int i = 0; i < chunkCount; i++) {
+            // 计算每个子列表的结束位置
+            // 如果有剩余的元素，先分配给前面的子列表
+            int end = start + chunkSize + (i < remainder ? 1 : 0);
+            List<T> chunk = new ArrayList<>(originalList.subList(start, end));
+            result.add(chunk);
+            // 更新下一段的起始位置
+            start = end;
+        }
+        return result;
+    }
+
     /**
      * 取交集
      *      使用HashSet快速检查元素是否存在于另一个集合中
